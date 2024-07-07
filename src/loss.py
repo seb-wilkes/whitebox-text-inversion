dab2f9cc895b3ece1c4300bfd2ed8ddaf28e3ba5
Seb Wilkes <61524039+seb-wilkes@users.noreply.github.com>
2024-07-07 02:49:27 +0100

    functional change

--- src/loss.py
+++ src/loss.py
@@ -7,3 +7,7 @@ def maximise_entropy_with_zero_penalty(activations, zero_penalty=0.1):
 	variances = torch.var(activations, dim=1).squeeze() # (batch_size)    
 	anti_sparsity = torch.mean(torch.abs(activations), dim=1).squeeze()
 	return variances - anti_sparsity * zero_penalty
+
+def return_maximise_entropy_with_zero_penalty(zero_penalty=0.1):
+    return maximise_entropy_with_zero_penalty(zero_penalty=zero_penalty)
+
